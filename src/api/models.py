416132b68beb2e72225320c994775c1b47bfd124
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-08-04 15:07:55 +0000

    commit

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.first_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token, get_jwt_identity, get_jwt, jwt_required
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
@@ -12,11 +13,20 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def user_login():
+    body=request.get_json()
+    if body["username"]!="arnaldo": 
+        return jsonify({"msg": "No autorizado"}), 401
+    if body["password"]!="123":
+        return jsonify({"msg": "No autorizado"}), 401
+    token = create_access_token(identity="arnaldo", additional_claims={"role":"admin"})
+    return jsonify({"token":token}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route("/userinfo", methods=['GET'])
+@jwt_required() 
+def user_info():
+    user=get_jwt_identity()
+    payload=get_jwt()
+    return jsonify({"user":user,"role":payload["role"]})
